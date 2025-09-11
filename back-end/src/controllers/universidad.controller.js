228aa84464e4d413dd7ef9bface58bdccf5e68ab
Franac0a <fbenitez0021@gmail.com>
2025-09-11 03:16:01 -0300

    endpoint para que las universidades vean las carreras que postean

--- back-end/src/controllers/universidad.controller.js
+++ back-end/src/controllers/universidad.controller.js
@@ -1,4 +1,5 @@
 import { UniversidadModel } from "../models/universidades.model.js";
+import { CarreraModel } from "../models/carreras.model.js";
 
 export const crearUniversidad = async (req, res) => {
   try {
@@ -22,14 +23,27 @@ export const crearUniversidad = async (req, res) => {
       userId: req.usuario.id,
     });
 
-    res
-      .status(201)
-      .json({
-        mensaje: "Universidad creada correctamente.",
-        universidad: nuevaUniversidad,
-      });
+    res.status(201).json({
+      mensaje: "Universidad creada correctamente.",
+      universidad: nuevaUniversidad,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ mensaje: "Error al crear universidad." });
+  }
+};
+
+export const obtenerMisCarreras = async (req, res) => {
+  try {
+    const universityId = req.usuario.id;
+
+    const carreras = await CarreraModel.findAll({
+      where: { universityId },
+    });
+
+    res.status(200).json(carreras);
+  } catch (error) {
+    console.error("Error al obtener mis carreras:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
 };
